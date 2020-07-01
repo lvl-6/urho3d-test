0bd67b6d4949027f8eeb38c8521c198ca71f53e8
John S Carignani <john@carignani.co.uk>
2020-07-01 16:14:50 +0100

    main.cpp now contains a main loop! We have a blank fullscreen program.

--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,52 @@
-#include <iostream>
+#include <Urho3D/Engine/Application.h>
+#include <Urho3D/Engine/Engine.h>
+#include <Urho3D/Input/InputEvents.h>
 
+using namespace Urho3D;
+
+class FirstApp : public Application
+{
+public:
+	FirstApp(Context* context) :
+		Application(context)
+	{
+	}
+
+	virtual void Setup()
+	{
+		// Called before engine initialization. engineParameters_ member variable can be modified here.
+	}
+
+	virtual void Start()
+	{
+		// Called after engine initialization. Setup application & subscribe to events here.
+		SubscribeToEvent(E_KEYDOWN, URHO3D_HANDLER(FirstApp, HandleKeyDown));
+	}
+
+	virtual void Stop()
+	{
+		// Perform optional cleanup after main loop has terminated
+	}
+
+	void HandleKeyDown(StringHash eventType, VariantMap& eventData)
+	{
+		using namespace KeyDown;
+
+		// Check for ESC pressed. Note the engine_ member variable for convenient access to the Engine object.
+		// ESC is our exit key, of course.
+		int key = eventData[P_KEY].GetInt();
+		if (key == KEY_ESCAPE)
+			engine_->Exit();
+	}
+	
+};
+
+
+URHO3D_DEFINE_APPLICATION_MAIN(FirstApp)
+
+/*
 int main()
 {
 	return 0;
 }
+*/
